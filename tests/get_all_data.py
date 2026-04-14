13141d6cca7dbcdd6ee60301bdc6a3c87d7ab570
YanzhenHuang <huangyanzhen0108@163.com>
2026-04-14 07:07:54 +0000

    fix: Fix data fetching program that gets wrong id.

--- tests/get_all_data.py
+++ tests/get_all_data.py
@@ -12,19 +12,18 @@ total = client.get_collection_stats(collection_name=COLLECTION_NAME)["row_count"
 print(f"Row Count: {total}")
 
 with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
-    for offset in range(0, total, BATCH_SIZE):
-        res = client.query(
-            collection_name=COLLECTION_NAME,
-            filter=f"id >= {offset}",
-            output_fields=ORDERED_FIELDS,
-            limit=BATCH_SIZE
-        )
+    res = client.query(
+        collection_name=COLLECTION_NAME,
+        # filter=f"",
+        output_fields=ORDERED_FIELDS,
+        # limit=BATCH_SIZE
+        limit=total
+    )
 
-        # File write
-        for item in res:
-            ordered_item = dict([(field, item[field]) for field in ORDERED_FIELDS])
-            f.write(json.dumps(item, ensure_ascii=False) + "\n")
+    # File write
+    for item in res:
+        f.write(json.dumps(item, ensure_ascii=False) + "\n")
 
-        print(f"Output: {min(offset + BATCH_SIZE, total)} / {total}")
+    # print(f"Output: {min(offset + BATCH_SIZE, total)} / {total}")
 
 print(f"\n Output successfully at：{OUTPUT_FILE}")
